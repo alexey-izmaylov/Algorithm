45926d2fa8cfd1baeaba9b4ce7f91b1e38e494db
alexey-izmaylov <aleksey.izmaylove@gmail.com>
2020-01-07 00:38:24 +0300

    implement merge sort

--- src/main/java/io/example/sort/MergeSort.java
+++ src/main/java/io/example/sort/MergeSort.java
@@ -1,15 +1,72 @@
 package io.example.sort;
 
-import java.util.ArrayList;
+import static java.lang.Math.min;
+
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Queue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MergeSort<T extends Comparable<T>> implements Sort<T> {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(MergeSort.class);
+
   @Override
   public List<T> sort(Iterable<T> list) {
-    ArrayList<T> arrayList = new ArrayList<>();
+    List<T> arrayList = new LinkedList<>();
     list.forEach(arrayList::add);
-    arrayList.sort(null);
-    return arrayList;
+    return sort(arrayList, 0);
+  }
+
+  private List<T> sort(List<T> list, final int level) {
+    final int group = (int) Math.pow(2, level);
+    LOGGER.debug("level={} group={} list={}", level, group, list);
+
+    if (group < list.size()) {
+      List<T> newList = new LinkedList<>();
+      for (int from = 0; from < list.size(); from += 2 * group) {
+
+        int middle = min(from + group, list.size());
+        List<T> left = list.subList(from, middle);
+        int to = min(from + 2 * group, list.size());
+
+        LOGGER.debug("    index from {} to {}", from, to - 1);
+
+        if (middle == to) {
+          LOGGER.debug("    already merged group: {}", left);
+          newList.addAll(left);
+        } else {
+          LOGGER.debug("    merge left ({}..{}) with right ({}..{})", from, middle - 1, middle, to - 1);
+          newList.addAll(
+              merge(
+                  new LinkedList<>(left),
+                  new LinkedList<>(list.subList(middle, to))
+              ));
+        }
+      }
+      return sort(newList, level + 1);
+    } else {
+      return list;
+    }
+  }
+
+  private List<T> merge(LinkedList<T> left, LinkedList<T> right) {
+    List<T> list = new LinkedList<>();
+    Optional<T> optional;
+    while ((optional = pollMin(left, right)).isPresent()) {
+      list.add(optional.get());
+    }
+    LOGGER.debug("        sorted merged group: {}", list);
+    return list;
+  }
+
+  private Optional<T> pollMin(Queue<T> left, Queue<T> right) {
+    if (left.isEmpty() && right.isEmpty()) return Optional.empty();
+    if (left.isEmpty()) return Optional.of(right.poll());
+    if (right.isEmpty()) return Optional.of(left.poll());
+    if (left.peek().compareTo(right.peek()) < 0) return Optional.of(left.poll());
+    return Optional.of(right.poll());
   }
 }
--- src/test/java/io/example/sort/MergeSortTest.java
+++ src/test/java/io/example/sort/MergeSortTest.java
@@ -1,18 +1,53 @@
 package io.example.sort;
 
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.util.List;
-import org.junit.jupiter.api.Test;
 
 class MergeSortTest {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(MergeSortTest.class);
+
   @Test
   void sort() {
-    List<Integer> sorted = new MergeSort<Integer>().sort(List.of(2, 1, 3, 2));
+    List<Integer> sorted = new MergeSort<Integer>().sort(List.of(2, 1, 3, 4, 2));
     assertNotNull(sorted);
-    assertThat(sorted, contains(1, 2, 2, 3));
+    assertThat(sorted, contains(1, 2, 2, 3, 4));
+  }
+
+  @Test
+  void sort0() {
+    assertThat(new MergeSort<Integer>().sort(List.of()), empty());
+  }
+
+  @Test
+  void sort1000() {
+    sortN(1000);
+  }
+
+  @Test
+  void sortSizesFrom1To20() {
+    for (int i = 1; i < 20; i++) {
+      LOGGER.info("sort {} elements", i);
+      sortN(i);
+    }
+  }
+
+  private void sortN(int capacity) {
+    ArrayList<Integer> list = new ArrayList<>(capacity);
+    Random random = new Random();
+    for (int i = 0; i < capacity; i++) {
+      list.add(random.nextInt(10));
+    }
+    assertThat(new MergeSort<Integer>().sort(list), contains(new JdkSort<Integer>().sort(list).toArray()));
   }
 }
