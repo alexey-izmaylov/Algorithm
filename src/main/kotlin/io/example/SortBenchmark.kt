0b6e7061cf64045089c0443cfbbe30401a4204af
aliz0214 <alexey.izmaylov@netcracker.com>
2020-01-07 21:02:54 +0300

    minor refactoring

--- src/main/kotlin/io/example/SortBenchmark.kt
+++ src/main/kotlin/io/example/SortBenchmark.kt
@@ -22,19 +22,19 @@ internal fun <T : Comparable<T>> mergeSort(list: List<T>) {
     val millis = measureNanoTime {
         MergeSort<T>().sort(list)
     }.toDouble() / 1_000_000
-    println("mergeSort finished in: $millis milliseconds");
+    println("mergeSort finished in: $millis milliseconds")
 }
 
 internal fun <T : Comparable<T>> quickSort(list: List<T>) {
     val millis = measureNanoTime {
         QuickSort<T>().sort(list)
     }.toDouble() / 1_000_000
-    println("quickSort finished in: $millis milliseconds");
+    println("quickSort finished in: $millis milliseconds")
 }
 
 internal fun <T : Comparable<T>> jdkSort(list: List<T>) {
     val millis = measureNanoTime {
         JdkSort<T>().sort(list)
     }.toDouble() / 1_000_000
-    println("jdkSort finished in: $millis milliseconds");
+    println("jdkSort finished in: $millis milliseconds")
 }
